f6f7e765999d3a9af02365c218aae41e0f1510e2
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-07-10 23:41:41 +0530

    test payload

--- src/inv.js
+++ src/inv.js
@@ -3,13 +3,13 @@ const createapi = async(req,res)=>{
     const store = []
     const body = req.body
     console.log(body)
-    if(body.id || body.sku || body.units || body.cost || body.supplier || body.lat && body.lng){
+    if (!body.id || !body.sku || !body.units || !body.cost || !body.supplier || !body.lat || !body.lng){
         console.warn("please fill all the feilds")
         return{
             res: res.status(400).json({error: "Please fill all the fields"}),
         }
     }
-    store.push({body})
+    store.push(body)
     console.log(store)
     return res.status(200).json({
         message: "Data received successfully",
