cc8551691df88caff61b256ca3beeea074cfb317
vaibhav@14 <vaibhavpandey331@gmail.com>
2025-07-11 23:56:43 +0530

    test

--- src/inv.js
+++ src/inv.js
@@ -62,29 +62,29 @@ module.exports = async (req, res) => {
 };
 
 
-const createapi = async (req, res) => {
-  const store = [];
-  const body = req.body;
-  console.log(body);
-  if (
-    !body.id ||
-    !body.sku ||
-    !body.units ||
-    !body.cost ||
-    !body.supplier ||
-    !body.lat ||
-    !body.lng
-  ) {
-    console.warn("please fill all the feilds");
-    return {
-      res: res.status(400).json({ error: "Please fill all the fields" }),
-    };
-  }
-  store.push(body);
-  console.log(store);
-  return res.status(200).json({
-    message: "Data received successfully",
-    data: store,
-  });
-};
-module.exports = createapi;
+// const createapi = async (req, res) => {
+//   const store = [];
+//   const body = req.body;
+//   console.log(body);
+//   if (
+//     !body.id ||
+//     !body.sku ||
+//     !body.units ||
+//     !body.cost ||
+//     !body.supplier ||
+//     !body.lat ||
+//     !body.lng
+//   ) {
+//     console.warn("please fill all the feilds");
+//     return {
+//       res: res.status(400).json({ error: "Please fill all the fields" }),
+//     };
+//   }
+//   store.push(body);
+//   console.log(store);
+//   return res.status(200).json({
+//     message: "Data received successfully",
+//     data: store,
+//   });
+// };
+// module.exports = createapi;
